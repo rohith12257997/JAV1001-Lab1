9abfa826e1bc9410292bc53a7894b06e48407cae
rohith12257997 <90713925+rohith12257997@users.noreply.github.com>
2021-09-25 12:21:41 -0400

    Update Conversions.java
    Minor Changes Done On Code

--- Conversions.java
+++ Conversions.java
@@ -10,26 +10,24 @@ import java.util.Scanner;
 
 public class Conversions {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 		
-		boolean loop=true; // this is intialised to iterate the loop
+		boolean loop=true;
 		
-		while(loop=true)
+		while(loop!=false) // change this
 		{
 		System.out.println("\nEnter your expression\n"); //Asking the input from the user
 		
-		System.out.println(" 1: Kms to Mi\n 2: Mi to Kms\n 3: Cms to in\n 4: In to Cms\n 5: Kgs to Lbs\n 6: Lbs to Kgs\n 7: Exit \n"); //Giving the options to User to select one 
+		System.out.println(" KM: Kms to Mi\n MI: Mi to Kms\n CM: Cms to in\n IN: In to Cms\n KG: Kgs to Lbs\n LB: Lbs to Kgs\n E: Exit \n"); //Giving the options to User to select one 
 		
 		Scanner sc = new  Scanner(System.in);          //This is used to take the input from the keyboard from the user
-		int expression = sc.nextInt();                // Storing the input given from the user to a variable
+		String expression = sc.next();                // Storing the input given from the user to a variable
 		
 		
 		// Using the Switch expression to select the Option 
 		switch(expression) {
 		
-		case 1:
-			// Kms to Mi
-				
+		case "KM":
 			System.out.println("Enter your Kms to convert it into Mi \n");
 			int Kms_value=sc.nextInt();
 			double final_result=Kms_value*0.62;
@@ -37,9 +35,8 @@ public class Conversions {
 			
 			break;
 		
-		case 2:
-			// Mi to Kms
-				
+		case "MI":
+			
 			System.out.println("Enter your Mi to convert it into Kms \n");
 			int Mi_value=sc.nextInt();
 			double final_result1=Mi_value*1.61;
@@ -49,9 +46,8 @@ public class Conversions {
 			break;
 			
 			
-		case 3:
-			// Cm to In
-				
+		case "CM":
+			
 			System.out.println("Enter your Cm to convert it into In \n");
 			int Cm_value=sc.nextInt();
 			double final_=Cm_value*0.39;
@@ -61,9 +57,8 @@ public class Conversions {
 			break;
 			
 			
-		case 4:
-			// In to Cm
-				
+		case "IN":
+			
 			System.out.println("Enter your In to convert it into Cms \n");
 			int In_value=sc.nextInt();
 			double final_1=In_value*2.54;
@@ -73,43 +68,41 @@ public class Conversions {
 			break;
 			
 			
-		case 5:
-			// Kgs to lbs
-				
+		case "KG":
+			
 			System.out.println("Enter your Kgs to convert it into lbs\n");
 			int Kgs_value=sc.nextInt();
 			double final_2=Kgs_value*2.2;
 			System.out.println(Kgs_value + " Kg = " + final_2 + " Lb");
 			break;
 			
-		case 6:
-			
-			// lbs to Kgs
-				
-			System.out.println("Enter your Lbs to convert it into Kgs\n");
+		case "LB":
+			System.out.println("Enter your Lbs to convert it into Kms\n");
 			int Lbs_value=sc.nextInt();
 			double final_Lbs=Lbs_value*2.2;
 			System.out.println(Lbs_value + " Kg = " + final_Lbs + " Lb");
 			break;
 			
-		case 7:
+		case "E":
 			
 			// Using this case to iterate the while loop
 			
-			System.out.println("Your Applicatin is Closed...Thank You\n");
+			System.out.println("Your Application is Closed...Thank You.... :) \n"); // change this spelling.....
 			loop=false;
+		
+			/*
+			 * //Closing the Scanner class sc.close();
+			 */
 			break;
 			
 			default:
 				// This default is used when the user enters other than the given option
-				
 				System.out.println("Entered Option is not from the given list, please try again.........\n");
+				
 				break;
 		
 		}
-		
-		// Closing the Scanner Class
-		sc.close();
+	
 		}
 		
 
